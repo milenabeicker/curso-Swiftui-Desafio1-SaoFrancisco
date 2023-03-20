76e47be836dcd0238cf5b31a3b143dc3ca873cac
Milena Kessia <milenakessia87@gmail.com>
2023-03-20 19:02:33 -0300

    Initial Commit

---
+++ Desafio1-SaoFrancisco/Desafio1_SaoFranciscoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio1_SaoFranciscoApp.swift
+//  Desafio1-SaoFrancisco
+//
+//  Created by Milena Kessia on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Desafio1_SaoFranciscoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
